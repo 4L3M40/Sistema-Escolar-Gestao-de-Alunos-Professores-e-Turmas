052eec15107210db3e6be14cefca9cfc9ba250ec
Evandro <evandrobch@hotmail.com>
2024-11-05 11:33:43 -0300

    Ok

--- src/main/java/org/senai/Main.java
+++ src/main/java/org/senai/Main.java
@@ -9,7 +9,7 @@ import org.senai.model.Turma;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] ignoredArgs) {
         // Criando os DAOs
         AlunoDAO alunoDAO = new AlunoDAO();
         ProfessorDAO professorDAO = new ProfessorDAO();
