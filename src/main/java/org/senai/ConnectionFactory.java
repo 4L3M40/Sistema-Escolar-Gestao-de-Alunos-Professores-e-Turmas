8e2ec99843b44689ee923a5f58ad0d45ba8a3009
Evandro Wagencknecht <evandrobch@hotmail.com>
2024-11-05 11:34:45 -0300

    Update ConnectionFactory.java

--- src/main/java/org/senai/ConnectionFactory.java
+++ src/main/java/org/senai/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     private static final String URL = "jdbc:mysql://localhost:3306/escola";
     private static final String USER = "root";
-    private static final String PASSWORD = "Needcarbon013@";
+    private static final String PASSWORD = "adimin";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
